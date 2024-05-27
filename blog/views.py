2c7dbb7e2928846629c35043a87e318e540ec53d
KoGyan <kogyan23@naver.com>
2024-05-28 00:22:52 +0900

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request,'blog/post_list.html',{})##디버깅 포인트
+    posts = Post.objects.filter(published_data__lte = timezone.now()).order_by('published_data')
+    return render(request,'blog/post_list.html',{'posts':posts})##디버깅 포인트
